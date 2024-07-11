27700f2d7e60c05b62acd378ee72a56283629f5a
Hunter Codexs <huntercodexs@gmail.com>
2024-07-11 18:04:54 -0300

    init: Initial commit on branch

--- src/main/java/com/huntercodexs/demo/config/Help4DevsAwsS3Config.java
+++ src/main/java/com/huntercodexs/demo/config/Help4DevsAwsS3Config.java
@@ -2,6 +2,7 @@ package com.huntercodexs.demo.config;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,14 +21,14 @@ public class Help4DevsAwsS3Config {
     @Bean
     public AmazonS3 s3Client() {
 
-        /*if (endpointUri == null || endpointUri.isEmpty()) {
+        if (endpointUri == null || endpointUri.isEmpty()) {
             endpointUri = "s3.amazonaws.com";
         }
 
         AwsClientBuilder.EndpointConfiguration endpointConfig = new AwsClientBuilder.EndpointConfiguration(
                 endpointUri,
                 region
-        );*/
+        );
 
         AWSCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
         //AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
