8765b63136c0ceb7063f3cce8ab2546cd748ce29
Hunter Codexs <huntercodexs@gmail.com>
2024-07-12 22:59:45 -0300

    feat: [stabled] [work] [tested] Added resources package (see in the project) + Bug file fixed when it is sent to S3 bucket

--- src/test/java/codexstester/test/unitary/Help4DevsAwsUnitaryTests.java
+++ src/test/java/codexstester/test/unitary/Help4DevsAwsUnitaryTests.java
@@ -5,30 +5,33 @@ import com.huntercodexs.demo.dto.Help4DevsAwsS3ResponseDto;
 import com.huntercodexs.demo.services.Help4DevsAwsS3Service;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
-import static codexstester.setup.datasource.MediaTests.ioFile;
+import static com.huntercodexs.demo.resources.Help4DevsFileHandlerService.byteFile;
 
 public class Help4DevsAwsUnitaryTests extends Help4DevsBridgeTests {
 
-    String filenameS3 = "5091310c-d833-4368-b3fa-47d976929b62.txt";
-    String filepath = "src/test/java/codexstester/setup/datasource";
+    String filenameS3 = "80327789-737d-4b8a-9aa6-869af5b8706f.doc";
+    String filepath = "src/main/resources";
+    //String fileType = "conf";
+    String fileType = "csv";
+    //String fileType = "doc";
+    //String fileType = "png";
 
     @Autowired
     Help4DevsAwsS3Service help4DevsAwsS3Service;
 
     @Test
     public void sendToS3Test() throws IOException {
-        byte[] fileContent = ioFile(filepath+"/selfie.txt").getBytes(StandardCharsets.UTF_8);
         MultipartFile multipartFile = new MockMultipartFile(
                 "file",
-                "selfie.txt",
-                "text/plain",
-                fileContent);
+                "attach."+fileType,
+                String.valueOf(MediaType.MULTIPART_FORM_DATA),
+                byteFile(filepath+"/attach."+fileType));
         Help4DevsAwsS3ResponseDto result = help4DevsAwsS3Service.uploadFile(multipartFile);
         System.out.println(result.getFilename());
         System.out.println(result.getMessage());
