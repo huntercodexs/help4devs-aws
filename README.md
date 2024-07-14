99c8c10a51301a14ccdc1cb0809e8de62deac83c
Hunter Codexs <huntercodexs@gmail.com>
2024-07-14 00:56:27 -0300

    init+feat: Initial commit on branch + New feature using aws-core

--- README.md
+++ README.md
@@ -38,15 +38,18 @@
 bucket.name=s3-help4devs-files
 cloud.aws.stack.auto=false
 cloud.aws.region.static=us-east-1
+cloud.aws.endpoint.uri=http://s3.localhost.localstack.cloud:4566/
 </pre>
 
 - Create aws credentials file
 
 <pre>
 [default]
-aws_access_key_id = {KEY}
-aws_secret_access_key = {SECRET}
+aws_access_key_id = test
+aws_secret_access_key = test
 </pre>
+
+> NOTE: The access_key and secret_access_key should be configured as test if the endpoint is pointing to localstack
 
 - Create the bucket in the AWS S3 Service
 
--- src/main/java/com/huntercodexs/demo/config/Help4DevsAwsCoreS3Config.java
+++ src/main/java/com/huntercodexs/demo/config/Help4DevsAwsCoreS3Config.java
@@ -2,6 +2,7 @@ package com.huntercodexs.demo.config;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,18 +12,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Help4DevsAwsCoreS3Config {
 
-
     @Value("${cloud.aws.region.static}")
     String region;
 
+    @Value("${cloud.aws.endpoint.uri:}")
+    String endpointUri;
+
     @Bean
     public AmazonS3 s3Client() {
+
+        if (endpointUri == null || endpointUri.isEmpty()) {
+            endpointUri = "s3.amazonaws.com";
+        }
+
+        AwsClientBuilder.EndpointConfiguration endpointConfig = new AwsClientBuilder.EndpointConfiguration(
+                endpointUri,
+                region
+        );
 
         AWSCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
 
         return AmazonS3ClientBuilder.standard()
                 .withCredentials(credentialsProvider)
-                .withRegion(region)
+                .withEndpointConfiguration(endpointConfig)
                 .build();
+
     }
 }
