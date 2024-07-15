c28cc779466afc045597770fb3b4d5e024cc23df
Hunter Codexs <huntercodexs@gmail.com>
2024-07-15 20:56:36 -0300

    init: Initial commit on branch

--- src/main/java/com/huntercodexs/demo/config/Help4DevsAwsSdkBomJmsSqsConfig.java
+++ src/main/java/com/huntercodexs/demo/config/Help4DevsAwsSdkBomJmsSqsConfig.java
@@ -2,6 +2,7 @@ package com.huntercodexs.demo.config;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,14 +15,26 @@ public class Help4DevsAwsSdkBomJmsSqsConfig {
     @Value("${cloud.aws.region.static}")
     String region;
 
+    @Value("${cloud.aws.endpoint.uri:}")
+    String endpointUri;
+
     @Bean
     public AmazonSQS amazonSQS() {
+
+        if (endpointUri == null || endpointUri.isEmpty()) {
+            endpointUri = "https://sqs."+region+".amazonaws.com/";
+        }
+
+        AwsClientBuilder.EndpointConfiguration endpointConfig = new AwsClientBuilder.EndpointConfiguration(
+                endpointUri,
+                region
+        );
 
         AWSCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
 
         return AmazonSQSClientBuilder.standard()
                 .withCredentials(credentialsProvider)
-                .withRegion(region)
+                .withEndpointConfiguration(endpointConfig)
                 .build();
     }
 }
