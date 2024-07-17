4bed578bf02561ff3e0a5870a96bc7e4883a829e
Hunter Codexs <huntercodexs@gmail.com>
2024-07-17 12:51:17 -0300

    init+feat: [stabled] [work] [tested] Added new branch using endpoint configuation

--- src/main/java/com/huntercodexs/demo/config/Help4DevsAwsSqsConfig.java
+++ src/main/java/com/huntercodexs/demo/config/Help4DevsAwsSqsConfig.java
@@ -10,11 +10,16 @@ import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
+import java.net.URI;
+
 @Configuration
 public class Help4DevsAwsSqsConfig {
 
     @Value("${cloud.aws.region.static}")
     String region;
+
+    @Value("${cloud.aws.endpoint.uri:}")
+    String endpointUri;
 
     @Bean
     public SqsTemplate sqsTemplate() {
@@ -25,10 +30,15 @@ public class Help4DevsAwsSqsConfig {
     @Primary
     public SqsAsyncClient sqsAsyncClient() {
 
+        if (endpointUri == null || endpointUri.isEmpty()) {
+            endpointUri = "https://sqs."+region+".amazonaws.com/";
+        }
+
         AwsCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create();
 
         return SqsAsyncClient.builder()
                 .credentialsProvider(credentialsProvider)
+                .endpointOverride(URI.create(endpointUri))
                 .region(Region.of(region))
                 .build();
 
--- src/test/java/codexstester/test/unitary/Help4DevsAwsCoreSqsUnitaryTests.java
+++ src/test/java/codexstester/test/unitary/Help4DevsAwsCoreSqsUnitaryTests.java
@@ -1,6 +1,7 @@
 package codexstester.test.unitary;
 
 import codexstester.setup.bridge.Help4DevsBridgeTests;
+import com.huntercodexs.demo.service.Help4DevsAwsSqsListener;
 import com.huntercodexs.demo.service.Help4DevsAwsSqsService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,23 @@ public class Help4DevsAwsCoreSqsUnitaryTests extends Help4DevsBridgeTests {
     @Autowired
     Help4DevsAwsSqsService help4DevsAwsSqsService;
 
+    @Autowired
+    Help4DevsAwsSqsListener help4DevsAwsSqsListener;
+
     @Test
     public void messagePublisherTest() {
-        help4DevsAwsSqsService.messagePublisher("test aws sqs 3");
+        help4DevsAwsSqsService.messagePublisher("test aws localhost.stack 7");
+    }
+
+    @Test
+    public void messageConsumerTest() {
+        System.out.println("Calling Listener");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Finished Listener");
     }
 
 }
